d94dc892f913542c6c32b82c8e4227bcc9938f24
LEE ZHE YU <tzef8220@gmail.com>
2022-05-08 15:03:14 +0800

    Initial Commit

---
+++ ModularExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ModularExample
+//
+//  Created by LEE ZHE YU on 2022/5/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
