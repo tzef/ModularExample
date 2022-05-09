a9f7635f97a1fe671843b96ba18d41d0ba91eaee
LEE ZHE YU <tzef8220@gmail.com>
2022-05-09 11:38:40 +0800

    Wrap completion in main queue into search service

--- ModularExample/Service/GithubSearchService.swift
+++ ModularExample/Service/GithubSearchService.swift
@@ -11,18 +11,32 @@ final class GithubSearchService {
     func search(keyword: String, completion: @escaping (Result) -> Void) {
         let request = GithubResource.search(keyword: "swift").request()
         APIManager.shared.request(request) { result in
+            guard let self = self else {
+                return
+            }
+
             switch result {
             case let .success((data, _)):
                 do {
                     let response = try JSONDecoder(
                         dateStrategy: .iso8601
                     ).decode(GithubSearchResponse.self, from: data)
-                    completion(.success(response.searchModel))
+                    self.performOnMainQueue(completion(.success(response.searchModel)))
                 } catch {
-                    completion(.failure(error))
+                    self.performOnMainQueue(completion(.failure(error)))
                 }
             case let .failure(error):
-                completion(.failure(error))
+                self.performOnMainQueue(completion(.failure(error)))
+            }
+        }
+    }
+
+    func performOnMainQueue(_ closure: @escaping @autoclosure (() -> Void)) {
+        if Thread.isMainThread {
+            closure()
+        } else {
+            DispatchQueue.main.async {
+                closure()
             }
         }
     }
