9d7c093a62a75e6fadff8bfafe7dfec4490406c9
LEE ZHE YU <tzef8220@gmail.com>
2022-05-22 07:22:27 +0800

    Add empty search framework

---
+++ ModuleSearch/ModuleSearch/ModuleSearch.h
@@ -0,0 +1,18 @@
+//
+//  ModuleSearch.h
+//  ModuleSearch
+//
+//  Created by LEE ZHE YU on 2022/5/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ModuleSearch.
+FOUNDATION_EXPORT double ModuleSearchVersionNumber;
+
+//! Project version string for ModuleSearch.
+FOUNDATION_EXPORT const unsigned char ModuleSearchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModuleSearch/PublicHeader.h>
+
+
