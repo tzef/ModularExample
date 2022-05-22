4588413405359594208fbacafa6d000a86597659
LEE ZHE YU <tzef8220@gmail.com>
2022-05-22 21:39:12 +0800

    Copy divider view to UI module

---
+++ ModuleUI/ModuleUI/Component/Separator/HorizontalDivider.swift
@@ -0,0 +1,31 @@
+//
+//  HorizontalDivider.swift
+//  ModuleUI
+//
+
+import UIKit
+
+final class HorizontalDivider: UIView {
+    var height: CGFloat = 1 {
+        didSet {
+            layer.cornerRadius = height / 2
+        }
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupView() {
+        backgroundColor = UIColor.systemGroupedBackground
+    }
+
+    override var intrinsicContentSize: CGSize {
+        CGSize(width: UIView.noIntrinsicMetric, height: height)
+    }
+}
