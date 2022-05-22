0b5eb71f71a1045bf0fc51cc3dbe982b0f72e12f
LEE ZHE YU <tzef8220@gmail.com>
2022-05-22 21:37:26 +0800

    Add empty UI module framework

---
+++ ModuleUI/ModuleUI/ModuleUI.h
@@ -0,0 +1,16 @@
+//
+//  ModuleUI.h
+//  ModuleUI
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ModuleUI.
+FOUNDATION_EXPORT double ModuleUIVersionNumber;
+
+//! Project version string for ModuleUI.
+FOUNDATION_EXPORT const unsigned char ModuleUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModuleUI/PublicHeader.h>
+
+
