654edbf26d56b07269ac5bd9dd415845bb6b87bd
LEE ZHE YU <tzef8220@gmail.com>
2022-05-22 22:00:16 +0800

    Add empty ModuleCore for sharing files

---
+++ ModuleCore/ModuleCore/ModuleCore.h
@@ -0,0 +1,16 @@
+//
+//  ModuleCore.h
+//  ModuleCore
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ModuleCore.
+FOUNDATION_EXPORT double ModuleCoreVersionNumber;
+
+//! Project version string for ModuleCore.
+FOUNDATION_EXPORT const unsigned char ModuleCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModuleCore/PublicHeader.h>
+
+
