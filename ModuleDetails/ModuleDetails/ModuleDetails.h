b4ccec5b26dc943ddf8af1cd34104a892691b9a6
LEE ZHE YU <tzef8220@gmail.com>
2022-05-22 20:41:32 +0800

    Add empty module details framework

---
+++ ModuleDetails/ModuleDetails/ModuleDetails.h
@@ -0,0 +1,18 @@
+//
+//  ModuleDetails.h
+//  ModuleDetails
+//
+//  Created by LEE ZHE YU on 2022/5/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ModuleDetails.
+FOUNDATION_EXPORT double ModuleDetailsVersionNumber;
+
+//! Project version string for ModuleDetails.
+FOUNDATION_EXPORT const unsigned char ModuleDetailsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModuleDetails/PublicHeader.h>
+
+
