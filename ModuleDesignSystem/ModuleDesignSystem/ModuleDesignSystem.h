d50a75f13cf5b4b52229305e611f7609cfefd806
LEE ZHE YU <tzef8220@gmail.com>
2022-06-11 22:14:24 +0800

    Add interface module: ModuleDesignSystem

---
+++ ModuleDesignSystem/ModuleDesignSystem/ModuleDesignSystem.h
@@ -0,0 +1,16 @@
+//
+//  ModuleDesignSystem.h
+//  ModuleDesignSystem
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ModuleDesignSystem.
+FOUNDATION_EXPORT double ModuleDesignSystemVersionNumber;
+
+//! Project version string for ModuleDesignSystem.
+FOUNDATION_EXPORT const unsigned char ModuleDesignSystemVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModuleDesignSystem/PublicHeader.h>
+
+
